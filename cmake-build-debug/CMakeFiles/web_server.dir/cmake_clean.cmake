7b13d010cb6bad86485589d5b5d2e9afaccccf3a
Brian Ruff <thelonewolf198927@gmail.com>
2019-01-28 18:56:57 -0500

    web server in C

---
+++ cmake-build-debug/CMakeFiles/web_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/web_server.dir/main.c.o"
+  "web_server.pdb"
+  "web_server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/web_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
